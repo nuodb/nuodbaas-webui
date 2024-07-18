c74afa6c7d20b14c58e2c512c6f5195146c34748
Andreas Goelzer (NuoDB) <139239314+agoelzer@users.noreply.github.com>
2024-07-18 09:01:15 -0400

    change port 80 -> 8080 (#6)

--- docker/production/files/entrypoint.sh
+++ docker/production/files/entrypoint.sh
@@ -9,7 +9,7 @@ fi
 if [ "${NUODB_COCKPIT_PATH_PREFIX}" != "" ] ; then
     find /usr/share/nginx/html -type f | while read line; do
         sed -i "s:\"/ui/:\"/${NUODB_COCKPIT_PATH_PREFIX}/:g" ${line}
-        sed -i "s:\"/ui\":\"/${NUODB_COCKPIT_PATH_PREFIX}\"/:g" ${line}
+        sed -i "s:\"/ui\":\"/${NUODB_COCKPIT_PATH_PREFIX}\":g" ${line}
     done
     mv /usr/share/nginx/html/ui "/usr/share/nginx/html/${NUODB_COCKPIT_PATH_PREFIX}"
 fi
