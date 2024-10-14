295b00914299ae51b0d9c8675160927bf2b21cdb
Andreas Goelzer <agr22@3ds.com>
2024-10-14 14:19:46 +0000

    fix password label typo

--- ui/src/components/pages/LoginForm.tsx
+++ ui/src/components/pages/LoginForm.tsx
@@ -45,7 +45,7 @@ export default function LoginForm({ setIsLoggedIn }: Props) {
                     <div className="fields">
                         <TextField required data-testid="organization" label="Organization" value={organization} onChange={(event) => setOrganization(event.target.value)} />
                         <TextField required data-testid="username" label="Username" value={username} onChange={(event) => setUsername(event.target.value)} />
-                        <TextField required data-testid="password" type="password" label="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+                        <TextField required data-testid="password" type="password" label="Password" value={password} onChange={(event) => setPassword(event.target.value)} />
                         {error && <h3 data-testid="error_message" style={{ color: "red" }}>{error}</h3>}
                         <Button data-testid="login_button" variant="contained" type="submit" onClick={handleLogin}>Login</Button>
                     </div>
