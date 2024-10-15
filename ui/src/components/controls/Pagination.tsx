dde3395d82f6a423bf4cf1d39299b9d466412c5d
Andreas Goelzer <agr22@3ds.com>
2024-10-15 19:37:10 +0000

    add Pagination/Menu wrapper

---
+++ ui/src/components/controls/Pagination.tsx
@@ -0,0 +1,44 @@
+// (C) Copyright 2024 Dassault Systemes SE.  All Rights Reserved.
+
+import { isMaterial } from '../../utils/Customizations';
+import { Pagination as MuiPagination, Stack as MuiStack } from '@mui/material';
+
+export type PaginationProps = {
+    "data-testid"?: string,
+    count: number,
+    page: number,
+    setPage: (page: number) => void
+}
+export default function Pagination(props: PaginationProps): JSX.Element {
+    const { count, page, setPage } = props;
+
+    if (isMaterial()) {
+        return <MuiStack spacing={2} style={{ alignItems: "center" }}>
+            <MuiPagination count={count} page={page} onChange={(event, page) => {
+                props.setPage(page);
+            }} />
+        </MuiStack>
+    }
+    else {
+        return <div className="NuoPagination">
+            <div className="NuoPaginationItem NuoPaginationItemFirst" onClick={() => setPage(1)}>&lt;</div>
+            {Array(count).fill(1).map((_, index) => {
+                let className = "NuoPaginationItem";
+                if (page === index + 1) {
+                    className += " NuoPaginationItemSelected";
+                }
+                if (page === 1) {
+                    className += " NuoPaginationItemFirst";
+                }
+                if (page === count) {
+                    className += " NuoPaginationItemLast";
+                }
+
+                return <div key={index} className={className} onClick={() => {
+                    setPage(index + 1)
+                }}>{index + 1}</div>
+            })}
+            <div className="NuoPaginationItem NuoPaginationItemLast" onClick={() => setPage(count)}>&gt;</div>
+        </div>;
+    }
+}
