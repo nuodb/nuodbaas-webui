62e16721967c8d54619c5728113839ce1e6ebd89
Andreas Goelzer (NuoDB) <139239314+agoelzer@users.noreply.github.com>
2024-12-02 06:02:51 -0500

    save built WebUI static files into gh-pages repo (#41)

--- docker/production/files/entrypoint.sh
+++ docker/production/files/entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 # (C) Copyright 2024 Dassault Systemes SE.  All Rights Reserved.
 
+if [ "$1" == "tgz_static" ] ; then
+    echo "Exctract static files from image"
+    cd /usr/share/nginx/html
+    tar -czf - -C /usr/share/nginx/html *
+    exit $?
+fi
+
 if [ "${NUODB_CP_REST_URL}" != "" ] ; then
     find /usr/share/nginx/html -type f | while read line; do
         sed -i "s#___NUODB_CP_REST_URL___#${NUODB_CP_REST_URL}#g" ${line}
