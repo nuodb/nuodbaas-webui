502052185ac7b5b4a97b51fc0ea23d29d08a3de0
Andreas Goelzer <agr22@3ds.com>
2026-01-05 07:25:00 +0000

    fix copyright

--- ui/src/utils/Customizations.test.js
+++ ui/src/utils/Customizations.test.js
@@ -1,4 +1,4 @@
-// (C) Copyright 2024 Dassault Systemes SE.  All Rights Reserved.
+// (C) Copyright 2024-2026 Dassault Systemes SE.  All Rights Reserved.
 
 import { expect, test } from 'vitest'
 import { evaluate, splitFormulaIntoParts } from './Customizations';
