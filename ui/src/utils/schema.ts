e03d8afe256ee870237afd8c0468f59cb3c078c8
Andreas Goelzer <agr22@3ds.com>
2026-01-13 18:10:32 +0000

    fix copyright

--- ui/src/utils/schema.ts
+++ ui/src/utils/schema.ts
@@ -1,4 +1,4 @@
-// (C) Copyright 2024-2025 Dassault Systemes SE.  All Rights Reserved.
+// (C) Copyright 2024-2026 Dassault Systemes SE.  All Rights Reserved.
 
 import Toast from "../components/controls/Toast";
 import { Rest } from "../components/pages/parts/Rest";
