931eb05967d241e6eff8f1b36a1b74b4513d2c6e
Andreas Goelzer <agr22@3ds.com>
2026-04-29 11:58:48 +0000

    make new reagion files "prettier"

--- ui-test/advanced/regions.spec.ts
+++ ui-test/advanced/regions.spec.ts
@@ -9,52 +9,46 @@ import {
   retry,
   createUserUI,
 } from "../helpers/ui";
-import {
-  shortUnique,
-} from "../helpers/api";
+import { shortUnique } from "../helpers/api";
 import { expect, TestInfo, type Page } from "@playwright/test";
 
 async function createAndDeleteUser(page: Page) {
-    // check REST API is still functional by creating and deleting a user
-    const userName = await createUserUI(page);
-    await clickMenu(page, "projects");
-    await clickMenu(page, "users");
+  // check REST API is still functional by creating and deleting a user
+  const userName = await createUserUI(page);
+  await clickMenu(page, "projects");
+  await clickMenu(page, "users");
 
-    // Delete User via popup menu
-    await retry(async () => {
-      const menuCells = await waitTableElements(
-        page,
-        "list_resource__table",
-        "name",
-        userName,
-        "$ref",
-      );
-      expect(menuCells.length).toBe(1);
-      await clickPopupMenu(page, menuCells[0], "delete_button");
-      await page.getByTestId("dialog_button_yes").click();
-    });
-    await waitRestComplete(page);
-
+  // Delete User via popup menu
+  await retry(async () => {
+    const menuCells = await waitTableElements(
+      page,
+      "list_resource__table",
+      "name",
+      userName,
+      "$ref",
+    );
+    expect(menuCells.length).toBe(1);
+    await clickPopupMenu(page, menuCells[0], "delete_button");
+    await page.getByTestId("dialog_button_yes").click();
+  });
+  await waitRestComplete(page);
 }
 
 test.describe("Region Selector Tests", () => {
-
-  test("CRUD tests", async ({
-    restPage: page,
-  }) => {
+  test("CRUD tests", async ({ restPage: page }) => {
     await page.goto("/ui/region-selector-settings");
 
     // open and cancel "Add Region Entry" dialog
-    await page.getByRole('button', { name: 'Add' }).click();
-    await page.getByTestId('button.cancel').click();
+    await page.getByRole("button", { name: "Add" }).click();
+    await page.getByTestId("button.cancel").click();
 
     // create custom region with default values
     const defaultLabel = shortUnique("d");
-    await page.getByRole('button', { name: 'Add' }).click();
+    await page.getByRole("button", { name: "Add" }).click();
     await page.locator('input[name="name"]').fill(defaultLabel);
-    await page.locator('input[name="cp"]').fill('/api');
-    await page.locator('input[name="sql"]').fill('/api/sql');
-    await page.getByTestId('button.add').click();
+    await page.locator('input[name="cp"]').fill("/api");
+    await page.locator('input[name="sql"]').fill("/api/sql");
+    await page.getByTestId("button.add").click();
 
     // make active
     await page.getByTestId("make-active-" + defaultLabel).click();
@@ -65,25 +59,27 @@ test.describe("Region Selector Tests", () => {
     await page.goto("/ui/region-selector-settings");
 
     // delete newly created entry
-    await page.getByRole('button', { name: defaultLabel }).click();
+    await page.getByRole("button", { name: defaultLabel }).click();
     await page.locator('input[name="name"]').waitFor({ state: "visible" });
-    await page.getByTestId('button.delete').click();
+    await page.getByTestId("button.delete").click();
   });
 
-  test("test invalid URL's", async ({
-    restPage: page,
-  }) => {
+  test("test invalid URL's", async ({ restPage: page }) => {
     await page.goto("/ui/region-selector-settings");
 
     // create custom region with invalid values
     const defaultLabel = shortUnique("d");
-    await page.getByRole('button', { name: 'Add' }).click();
+    await page.getByRole("button", { name: "Add" }).click();
     await page.locator('input[name="name"]').fill(defaultLabel);
-    await page.locator('input[name="cp"]').fill('/api-invalid');
-    await page.locator('input[name="sql"]').fill('/api-invalid/sql');
-    await page.getByTestId('button.add').click();
+    await page.locator('input[name="cp"]').fill("/api-invalid");
+    await page.locator('input[name="sql"]').fill("/api-invalid/sql");
+    await page.getByTestId("button.add").click();
 
     // the error message should show up twice (for the "cp" and "sql" fields)
-    await expect(page.getByText('Unable to connect: AxiosError: Request failed with status code 404')).toHaveCount(2);
+    await expect(
+      page.getByText(
+        "Unable to connect: AxiosError: Request failed with status code 404",
+      ),
+    ).toHaveCount(2);
   });
 });
--- ui/src/components/pages/RegionSettingsMenu.tsx
+++ ui/src/components/pages/RegionSettingsMenu.tsx
@@ -5,65 +5,74 @@ import Auth from "../../utils/auth";
 import { useNavigate } from "react-router-dom";
 import { IconButton, Tooltip } from "@mui/material";
 import Menu from "../controls/Menu";
-import PublicIcon from '@mui/icons-material/Public';
-import CheckIcon from '@mui/icons-material/Check';
-import EditIcon from '@mui/icons-material/Edit';
+import PublicIcon from "@mui/icons-material/Public";
+import CheckIcon from "@mui/icons-material/Check";
+import EditIcon from "@mui/icons-material/Edit";
 import Button from "../controls/Button";
 
-function RegionSettingsMenu({t}:{t:any}) {
-    const navigate = useNavigate();
+function RegionSettingsMenu({ t }: { t: any }) {
+  const navigate = useNavigate();
 
-    if (!localStorage.getItem("regions")) {
-        return null;
-    }
+  if (!localStorage.getItem("regions")) {
+    return null;
+  }
 
-    const items = [
-        {
-            label: t("form.editRegionSettings.label.defaultRegion"),
-            icon: !Auth.getRegions().find(region => region.active) ? <CheckIcon /> : undefined,
-            id: "region.default",
-            "data-testid": "region.default",
-            onClick: () => {
-                Auth.setRegions(Auth.getRegions().map((region) => ({ ...region, active: false })));
-                window.location.reload();
-                return true;
-            }
-        },
-        ...Auth.getRegions().map((region, index) => ({
-            label: region.name,
-            icon: region.active ? <CheckIcon /> : undefined,
-            id: region.name,
-            "data-testid": region.name,
-            onClick: () => {
-                Auth.setRegions(Auth.getRegions().map((region, idx) => ({ ...region, active: idx === index })));
-                window.location.reload();
-                return true;
-            }
-        })),
-        {
-            label: t("form.editRegionSettings.label.editRegions"),
-            icon: <EditIcon />,
-            id: "edit.region.selector",
-            "data-testid": "edit.region.selector",
-            hasSeparator: true,
-            onClick: () => {
-                navigate("/ui/region-selector-settings");
-                return true;
-            }
-        }
-    ];
+  const items = [
+    {
+      label: t("form.editRegionSettings.label.defaultRegion"),
+      icon: !Auth.getRegions().find((region) => region.active) ? (
+        <CheckIcon />
+      ) : undefined,
+      id: "region.default",
+      "data-testid": "region.default",
+      onClick: () => {
+        Auth.setRegions(
+          Auth.getRegions().map((region) => ({ ...region, active: false })),
+        );
+        window.location.reload();
+        return true;
+      },
+    },
+    ...Auth.getRegions().map((region, index) => ({
+      label: region.name,
+      icon: region.active ? <CheckIcon /> : undefined,
+      id: region.name,
+      "data-testid": region.name,
+      onClick: () => {
+        Auth.setRegions(
+          Auth.getRegions().map((region, idx) => ({
+            ...region,
+            active: idx === index,
+          })),
+        );
+        window.location.reload();
+        return true;
+      },
+    })),
+    {
+      label: t("form.editRegionSettings.label.editRegions"),
+      icon: <EditIcon />,
+      id: "edit.region.selector",
+      "data-testid": "edit.region.selector",
+      hasSeparator: true,
+      onClick: () => {
+        navigate("/ui/region-selector-settings");
+        return true;
+      },
+    },
+  ];
 
-    return <Menu data-testid="region.menu"
-        align="right"
-        items={items}
-    >
-        <Tooltip title={t("hint.regionSelector")}>
-            <Button variant="text" onClick={()=>{}}>
-                <PublicIcon fontSize="large" />
-                {items.find(item=>item.icon !== undefined)?.label || t("form.editRegionSettings.label.defaultRegion")}
-            </Button>
-        </Tooltip>
-    </Menu>;
+  return (
+    <Menu data-testid="region.menu" align="right" items={items}>
+      <Tooltip title={t("hint.regionSelector")}>
+        <Button variant="text" onClick={() => {}}>
+          <PublicIcon fontSize="large" />
+          {items.find((item) => item.icon !== undefined)?.label ||
+            t("form.editRegionSettings.label.defaultRegion")}
+        </Button>
+      </Tooltip>
+    </Menu>
+  );
 }
 
-export default withTranslation()(RegionSettingsMenu)
+export default withTranslation()(RegionSettingsMenu);
