6df7f07186e719f28d7b5a45c9598730b0b2bd4d
Andreas Goelzer <agr22@3ds.com>
2026-06-04 18:05:36 +0000

    remove debug statements

--- ui-test/smoke/resources.spec.ts
+++ ui-test/smoke/resources.spec.ts
@@ -15,34 +15,17 @@ test.describe("ResourcesTest (smoke)", () => {
     restPage: page,
   }) => {
     // Create in dependency order
-    console.log("STEP1");
     const user = await createUserUI(page);
-    console.log("STEP2");
-    await sleep(100);
     const project = await createProjectUI(page);
-    console.log("STEP3");
-    await sleep(100);
     const db = await createDatabaseUI(page, project);
-    console.log("STEP4");
-    await sleep(100);
     const backup = await createBackupUI(page, project, db);
-    console.log("STEP5");
 
-    await sleep(1000); // TODO(agr22)
+    await sleep(100); // TODO(agr22)
 
     // Delete in reverse dependency order
-    console.log("STEP6");
     await deleteResourceUI(page, "backups", backup);
-    console.log("STEP7");
-    await sleep(100);
     await deleteResourceUI(page, "databases", db);
-    console.log("STEP8");
-    await sleep(100);
     await deleteResourceUI(page, "projects", project);
-    console.log("STEP9");
-    await sleep(100);
     await deleteResourceUI(page, "users", user);
-    console.log("STEP10");
-    await sleep(100);
   });
 });
