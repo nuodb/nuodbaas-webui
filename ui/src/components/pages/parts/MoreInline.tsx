14c85b14d30af7b56e78e989305ce035fbfcd97e
Andreas Goelzer (NuoDB) <139239314+agoelzer@users.noreply.github.com>
2026-06-09 08:43:06 -0400

    fix copy on Windows (had missing newlines) (#150)

--- ui/src/components/pages/parts/MoreInline.tsx
+++ ui/src/components/pages/parts/MoreInline.tsx
@@ -11,6 +11,10 @@ type MoreInlineProps = {
 export default function MoreInline({ value, t }: MoreInlineProps) {
   const [expanded, setExpanded] = useState<boolean>(false);
 
+  if (!value) {
+    return value;
+  }
+
   let strValue = String(value);
   let moreValue = "";
   if (strValue.indexOf("\n") !== -1) {
@@ -21,16 +25,23 @@ export default function MoreInline({ value, t }: MoreInlineProps) {
     moreValue = strValue.substring(80) + moreValue;
     strValue = strValue.substring(0, 80);
   }
+  if (expanded) {
+    return (
+      <span style={{ whiteSpace: "pre-wrap" }}>
+        {value.replace(/\n/g, "\r\n")}
+      </span>
+    );
+  }
   return (
     <>
-      {strValue}
+      {!expanded && strValue}
       <div
         className="NuoMoreValue"
         onClick={(element: React.MouseEvent<HTMLDivElement>) => {
           setExpanded(true);
         }}
       >
-        {expanded ? value : <> {t("text.more")}</>}
+        {t("text.more")}
       </div>
     </>
   );
